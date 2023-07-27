0df6214179aea702d82865a7f18e406dae2c5fc9
IuriiZalutkyi <yzalutskiy@gmail.com>
2023-07-27 20:05:04 +0200

    Days3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,23 @@
-﻿var name = "Iurii";
-var age = 20;
-var sex = "Woman";
+﻿var name = "Kasper";
+var age = 18;
+var sex = "man";
 
 if (sex == "man")
 {
-    if  (age <30)
-    { Console.WriteLine("Mezczyzna ponizej 30 lat"); }
-    else if (age<20)
-    { Console.WriteLine("Niepelnolliatek meczyzna"); }
-    else 
-    Console.WriteLine("Mezczyzna, meczyzna powyzej 30 lat  ");
+    if (age > 30)
+    {
+        Console.WriteLine(name + ", powyzej 30 lat");
+    }
+    else if (age > 20)
+    {
+        Console.WriteLine("Mezczyzna ponizej 30 lat");
+    }
+    else if (age < 20)
+    {
+        Console.WriteLine("Niepelnoliatek meczyzna");
+    }
 }
-
 else
 {
-    Console.WriteLine("Kobieta lat ");
+    Console.WriteLine("Kobieta lat " + age);
 }
