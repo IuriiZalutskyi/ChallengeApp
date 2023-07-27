692e0135c7ce8ec44f526a17b4db4bc40cfa9e9a
IuriiZalutkyi <yzalutskiy@gmail.com>
2023-07-27 21:20:22 +0200

    Domowe zadanie, dzien4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,23 +1,24 @@
-﻿var name = "Kasper";
+﻿var name = "Ewa";
 var age = 18;
 var sex = "man";
 
-if (sex == "man")
+if (sex == "woman")
 {
-    if (age > 30)
+    if (age <= 30)
     {
-        Console.WriteLine(name + ", powyzej 30 lat");
+        Console.WriteLine("Kobieta ponizej 30 lat");
     }
-    else if (age > 20)
+    else if (age == 33 && name == "Ewa")
     {
-        Console.WriteLine("Mezczyzna ponizej 30 lat");
-    }
-    else if (age < 20)
-    {
-        Console.WriteLine("Niepelnoliatek meczyzna");
+        Console.WriteLine("Ewa,  lat 33");
     }
 }
 else
 {
-    Console.WriteLine("Kobieta lat " + age);
+   if (age>=18)
+    { 
+        Console.WriteLine("Pelnolietni meczyzna");
+    }
+   else
+    Console.WriteLine("Niepelnoliatek meczyzna");
 }
