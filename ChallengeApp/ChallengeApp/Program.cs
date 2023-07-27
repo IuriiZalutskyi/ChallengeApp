08d136b95faa099e778f63ad66d4eefe448a1d0e
IuriiZalutkyi <yzalutskiy@gmail.com>
2023-07-27 15:11:11 +0200

    3 Days

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Iurii";
+var age = 20;
+var sex = "Woman";
+
+if (sex == "man")
+{
+    if  (age <30)
+    { Console.WriteLine("Mezczyzna ponizej 30 lat"); }
+    else if (age<20)
+    { Console.WriteLine("Niepelnolliatek meczyzna"); }
+    else 
+    Console.WriteLine("Mezczyzna, meczyzna powyzej 30 lat  ");
+}
+
+else
+{
+    Console.WriteLine("Kobieta lat ");
+}
