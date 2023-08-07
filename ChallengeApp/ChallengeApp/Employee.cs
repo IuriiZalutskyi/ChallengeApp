310f24046c535a2f9d4befdfb193b52cc87b28b2
IuriiZalutkyi <yzalutskiy@gmail.com>
2023-08-07 21:21:36 +0200

    Days6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+
+        private List<int> score = new List<int>();
+
+
+        public Employee(string name, string nazwisko, uint wiek)
+        {
+            this.Name = name;
+            this.Nazwisko = nazwisko;
+            this.Wiek = wiek;
+        }
+        public string Name { get; private set; }
+        public string Nazwisko { get; private set; }
+        public uint Wiek { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+
+    }
+}
