6f4b78b52be76031ffaff80610adcf3dc6296c45
IuriiZalutkyi <yzalutskiy@gmail.com>
2023-08-09 19:03:22 +0200

    days8

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,35 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TypeWartosciowyInt()
+        {
+            //arrange
+            int number1 = 1;
+            int number2 = 2;
+            //act
+
+            //assert
+            Assert.AreNotEqual(number1, number2);
+
+        }
+        [Test]
+        public void TypeReferncijnyString()
+        {
+            //arrange
+            string user1 = "Kasper";
+            string user2 = "Kasper";
+            //act
+
+            //assert
+            Assert.AreEqual(user1, user2);
+
+        }
+
+        private Employee GetUser(string name)
+        {
+            return new Employee(name);
+        }
+    }
+}
