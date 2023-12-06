ca660af196214a1dc9eecbfd08bbd61df01b1f45
IuriiZalutkyi <yzalutskiy@gmail.com>
2023-12-06 17:25:37 +0100

    days10

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -9,14 +9,46 @@
             this.Name = name;
             this.Surname = surname;
         }
-
         public string Name { get; private set; }
         public string Surname { get; private set; }
-
-
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            //3.99
+            //3
+            int valueInInt = (int)grade;
+            float f = valueInInt;
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
+        }
+        public void AddGrade(double grade)
+        {
+            float valueinfloat = (float)grade;
+            this.AddGrade(valueinfloat);
+        }
+        
+        public void AddGrade(long grade)
+        {
+            float valueinfloat = (float)grade;
+            this.AddGrade(valueinfloat);
+        }
+
+
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
         }
         public Statistics GetStatistics()
         {
@@ -31,7 +63,6 @@
                 statistics.Min = Math.Min(statistics.Min, grade);
                 statistics.Avarege += grade;
             }
-
             statistics.Avarege = statistics.Avarege / this.grades.Count;
             return statistics;
         }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,11 +1,15 @@
 ﻿using ChallengeApp;
 
 var employee = new Employee("Adam", "Kasperski");
-employee.AddGrade(2);
+employee.AddGrade("4000");
+employee.AddGrade("Adam");
+employee.AddGrade(-5);
+employee.AddGrade(3000);
 employee.AddGrade(5);
 employee.AddGrade(6);
 
 var statistics = employee.GetStatistics();
+
 Console.WriteLine($"Avarege:{statistics.Avarege:N2}");
 Console.WriteLine($"Max:{statistics.Max}");
 Console.WriteLine($"Min:{statistics.Min}");
